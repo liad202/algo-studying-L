53a6ffb1a92d8572740d9583d989c116dd73e8ae
algo <visual@hermelin.ort.org.il>
2024-05-09 19:14:27 +0300

    i change

--- algo-studying-L/Program.cs
+++ algo-studying-L/Program.cs
@@ -6,7 +6,7 @@ namespace algo_studying_L
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Israel!");
         }
     }
 }
