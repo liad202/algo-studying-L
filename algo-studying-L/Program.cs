739e7c72bd1ffca6b4ea476fb406cc76a313e8a1
Administrator <Administrator@4115-001>
2024-05-09 18:56:19 +0300

    Add project files.

---
+++ algo-studying-L/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algo_studying_L
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
