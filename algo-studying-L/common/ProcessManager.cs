1deed4354a0b394be385c8a058b67c02ebc9150f
algo <visual@hermelin.ort.org.il>
2024-05-09 19:34:36 +0300

    run

---
+++ algo-studying-L/common/ProcessManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algo_studying_L.common
+{
+    public class ProcessManager
+    {
+        public static void RunProcess()
+        {
+            Console.WriteLine("Choose one option:1 - World | 2 - Israel | 0 - Exit");
+        }
+    }
+}
